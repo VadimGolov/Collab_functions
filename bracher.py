216beb47fd50face40a7aea7a80b3c4c2f9724a8
Вадим Голов <grafit-design@inbox.ru>
2025-05-01 15:08:23 +0500

    Добавил окно для выбора ветки

---
+++ bracher.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import ttk
+import subprocess
+
+
+def get_branches():
+    """
+    Получить список всех веток репозитория
+    """
+    all_branches = subprocess.check_output(['git', 'branch', '--list']).strip().decode('utf-8').split('\n')
+    return [one_branch.strip('* ').strip() for one_branch in all_branches]
+
+
+def select_branch(branch_set):
+    """
+    Создает окно для выбора ветки из списка branches и возвращает выбранную ветку
+    """
+    # Создание окна Tkinter
+    root = tk.Tk()
+    root.title('Выбор ветки')
+    root.configure(bg='#2E2E2E')  # Темная тема
+    root.geometry('380x140')
+    root.resizable(False, False)
+
+    # Лейбл
+    label = tk.Label(root, text='Выберите ветку  репозитория Collab_functions', font=('Arial', 12), fg='white', bg='#2E2E2E')
+    label.pack(pady=5)
+
+    # Список выбора веток
+    branch_var = tk.StringVar()
+    branch_var.set(branch_set[0])  # Установим первую ветку по умолчанию
+    branch_menu = ttk.Combobox(root, textvariable=branch_var, values=branch_set, font=('Arial', 11), state='readonly', width=30)
+    branch_menu.pack(pady=10)
+
+    # Кнопка для подтверждения выбора
+    def on_select():
+        selected_branch = branch_var.get()
+        root.quit()  # Закрыть окно после выбора
+        root.destroy()
+        return selected_branch
+
+    button = tk.Button(root, text='Выбрать', command=on_select, font=('Arial', 11), bg='#505050', fg='white')
+    button.pack(pady=10)
+
+    # Запуск окна
+    root.mainloop()
+
+    print(branch_var.get())
+
+    return branch_var.get()  # Возвращаем выбранную ветку
+
+#Программа
+branch_heap = get_branches()
+branch_to_checkout = select_branch(branch_heap)
