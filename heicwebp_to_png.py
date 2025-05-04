66c4079cefc182f754e5ef29ab71778ec71c09fc
Вадим Голов <grafit-design@inbox.ru>
2025-05-04 18:04:04 +0500

    Правлю heicwebp_to_png.py

--- heicwebp_to_png.py
+++ heicwebp_to_png.py
@@ -1,4 +1,4 @@
-def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
+def heicwebp_to_png(arguments: dict[str, str | bool]) -> dict[str, str]:
 
     from pathlib import Path
     from PIL import Image, UnidentifiedImageError
@@ -25,8 +25,10 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
 
     """
     # Проверяем, что arguments — это словарь
+    print(f'arguments {arguments}')
     if not isinstance(arguments, dict):
         message: str = 'Аргументы должны быть переданы в виде словаря'
+        print({'status': 'error', 'message': message})
         return {'status': 'error', 'message': message}
 
     # Проверка input_path
@@ -38,7 +40,7 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
         err_code: int = 1
     elif not Path(input_path).is_file():
         err_code: int = 2
-    elif not Path(input_path).suffix in ('.heic', '.webp'):
+    elif not Path(input_path).suffix in ('.heic', '.webp', '.HEIC', '.WEBP'):
         err_code: int = 3
     else:
         err_code: int = -1
@@ -49,12 +51,13 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
                               f'Файл {input_path} должен иметь тип .heic или .webp']
 
     if err_code != -1:
-        return {'status': 'error', 'message': input_error}
+        print({'status': 'error', 'message': input_error[err_code]})
+        return {'status': 'error', 'message': input_error[err_code]}
 
     # Проверка output_path
     output_path: str | None = arguments.get('output_path', None)
 
-    if output_path is None:
+    if output_path == '' or output_path is None:
         output_path: Path = Path(input_path).parent
     elif not isinstance(output_path, str):
         err_code: int = 0
@@ -67,11 +70,13 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
                                f'Папка: {output_path} не найдена или не является папкой']
 
     if err_code != -1:
-        return {'status': 'error', 'message': output_error}
+        print({'status': 'error', 'message': output_error[err_code]})
+        return {'status': 'error', 'message': output_error[err_code]}
 
     # Проверка overwrite
     overwrite: bool = arguments.get('overwrite', True)
     if not isinstance(overwrite, bool):
+        print({'status': 'error', 'message': '"overwrite" должен быть типа bool'})
         return {'status': 'error', 'message': '"overwrite" должен быть типа bool'}
 
     # Конвертация фото
@@ -82,6 +87,7 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
         src_image: Image = Image.open(input_path)
 
     except UnidentifiedImageError:
+        print({'status': 'error', 'message': f'Не удалось открыть файл {input_path}'})
         return {'status': 'error', 'message': f'Не удалось открыть файл {input_path}'}
 
     if overwrite:
@@ -102,4 +108,9 @@ def heicwebp_to_png(arguments: dict[str, str | list | bool]) -> dict[str, str]:
         src_image.save(output_image)
     finally:
         src_image.close()
+        print({'status': 'success', 'message': f'{output_image}'})
         return {'status': 'success', 'message': f'{output_image}'}
+
+if __name__ == '__main__':
+
+    heicwebp_to_png(arguments={'input_path': 'D:\Storage\Zerocoder\heic\IMG_4442.HEIC', 'output_path': 'D:\Storage\Zerocoder\heic\png', 'overwrite': True})
